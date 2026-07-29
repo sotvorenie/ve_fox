f2deeda8ae907e4e1869d7c86592e848d898adb2
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-07-29 14:32:36 +0300

    корректировка api запросов под node-сервер

--- src/api/save_time/saveTime.ts
+++ src/api/save_time/saveTime.ts
@@ -1,14 +1,9 @@
 import {SuccessResponse} from "@/types/success";
-import {SaveTime} from "@/types/saveTime";
 
-import {apiDelete, apiGet, apiPost} from "@api/index";
+import {apiDelete, apiPost} from "@api/index";
 
 export const apiSaveTime = (videoId: number, time: number): Promise<SuccessResponse> => {
     return apiPost(`/save_time/set/${videoId}`, {time})
-}
-
-export const apiGetSavedTime = (videoId: number): Promise<SaveTime> => {
-    return apiGet(`/save_time/get/${videoId}`)
 }
 
 export const apiDeleteSavedTime = (videoId: number): Promise<SuccessResponse> => {
